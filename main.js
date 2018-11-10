661107baa2ec189ec301a60874e9ba19b40391fc
mkbouallegue <43909330+mkbouallegue@users.noreply.github.com>
2018-11-10 15:48:47 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,32 @@
+function myFonction() {
+    var text = document.getElementById ('n').style.fontWeight ='bold';
+}
+function myStyle() {
+    var text = document.getElementById('n').style.fontStyle='italic';
+}
+function mySous() {
+    var text = document.getElementById('n').style.textDecoration='Underline';
+}
+function myCenter() {
+    var text = document.getElementById('n').style.textAlign='center';
+}
+
+function myFont1() {
+    var text = document.getElementById('n').style.fontFamily ='verdana';
+}
+
+function myFont2() {
+    var text = document.getElementById('n').style.fontFamily ='sans-serif';
+}
+function myFont3() {
+    var text = document.getElementById('n').style.fontFamily ='Gloria Hallelujah';
+}
+function myNumb1(){
+    var text = document.getElementById('n').style.fontSize = '40px';
+}
+function myNumb2(){
+    var text = document.getElementById('n').style.fontSize = '60px';
+}
+function myNumb3(){
+    var text = document.getElementById('n').style.fontSize = '70px';
+}
