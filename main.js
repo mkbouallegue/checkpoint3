74477839f9acb4341e5242b79e6db1d351736ac2
mkbouallegue <43909330+mkbouallegue@users.noreply.github.com>
2018-11-10 18:02:33 +0100

    commit2

--- main.js
+++ main.js
@@ -11,22 +11,12 @@ function myCenter() {
     var text = document.getElementById('n').style.textAlign='center';
 }
 
-function myFont1() {
-    var text = document.getElementById('n').style.fontFamily ='verdana';
+function myFont() {
+    var font = document.getElementById("listFonts").value
+    document.getElementById('n').style.fontFamily = font
 }
 
-function myFont2() {
-    var text = document.getElementById('n').style.fontFamily ='sans-serif';
-}
-function myFont3() {
-    var text = document.getElementById('n').style.fontFamily ='Gloria Hallelujah';
-}
-function myNumb1(){
-    var text = document.getElementById('n').style.fontSize = '40px';
-}
-function myNumb2(){
-    var text = document.getElementById('n').style.fontSize = '60px';
-}
-function myNumb3(){
-    var text = document.getElementById('n').style.fontSize = '70px';
+function myNumb() {
+    var numero = document.getElementById('listNumb').value
+    document.getElementById('n').style.fontSize = numero
 }
